6f23201c3879dea40eb330ad047244fe0a9dc5a2
Stephanie Nemeth <traumverloren@gmail.com>
2017-02-05 13:18:27 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
+import { createSore } from 'redux';
+import reducer from './reducers/index';
+
+const __REDUX_DEVTOOLS_EXTENSION__ = window.__REDUX_DEVTOOLS_EXTENSION__;
+// then we create a store, and we fold in our devtools, but only if it exists for our browser
+const store = createStore(reducer, __REDUX_DEVTOOLS_EXTENSION__ && __REDUX_DEVTOOLS_EXTENSION__());
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
